fe1c65fbf9d771c49b22ece3c697453660016871
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-07-01 17:08:59 -0300

    Criando main.py

--- bloco1/desafio05/main.py
+++ bloco1/desafio05/main.py
@@ -1,1 +1,2 @@
-# 05 Algoritmo de Rate Limiting (Token Bucket): Implemente em Python puro a lógica do algoritmo Token Bucket para controlar o limite de requisições por IP (ex: 5 requisições por minuto).
+# 05 Algoritmo de Rate Limiting (Token Bucket): Implemente em Python puro a lógica do algoritmo Token Bucket para controlar
+#  o limite de requisições por IP (ex: 5 requisições por minuto).
